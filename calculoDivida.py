0d0b39d28728bc2c44e24db97176e876db353522
numdez <76885866+numdez@users.noreply.github.com>
2024-03-11 17:14:16 -0300

    Update calculoDivida.py

--- calculoDivida.py
+++ calculoDivida.py
@@ -16,7 +16,7 @@ def round_up(x, place=2):
     context.rounding = original_rounding
     return float(rounded)
 
-def proximaFatura(aParcela, aFaltam, bParcela, bFaltam, cParcela, cFaltam, extra):
+def proximaFatura(aParcela, aFaltam, bParcela, bFaltam, cParcela, cFaltam, extra, extra2, extra3, extra4, acadMes):
     nextFatura = 0
     if aFaltam > 0:
         nextFatura += aParcela
@@ -26,7 +26,21 @@ def proximaFatura(aParcela, aFaltam, bParcela, bFaltam, cParcela, cFaltam, extra
         nextFatura += cParcela
     if extra > 0:
         nextFatura += extra
+    if extra2 > 0:
+        nextFatura += extra2
+    if extra3 > 0:
+        nextFatura += extra3
+    if extra4 > 0:
+        nextFatura += extra4
+    nextFatura += acadMes
     return nextFatura
+
+def parcelaCompra(valor, parcelas):
+    parcelado = []
+    for i in range(parcelas):
+        parcelado.append(valor/parcelas)
+    return parcelado
+
 
 
 meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
@@ -35,67 +49,72 @@ salarioBruto = 811.4
 chequeEspecial = 300
 livreCheque = 0
 
-taxaCancelada = 0
-taxaBanco = 50
+
 contaSky = 130
 contaCondo = 300
-contaSky2 = 0
-contaCondo2 = 0
 
-motherboard = 350 + 24
-moboCartao = 411 + 24
+
+motherboard = 338.24
+moboCartao = 338.24
 gabinete = 210
 cpu = 576
-fonte600w = 280 + 31
-f600wCartao = 329.5 + 31
-fonte650w = 288 + 34
-f650wCartao = 339 + 34
+fonte600w = 295
+'''fonte650w = 288 + 34
+f650wCartao = 339 + 34'''
+fonteAli = 237.92
 ram8 = 99.52
 impostoRam8 = 20.38
 totalRam8 = round_up(ram8 + impostoRam8)
-ram16 = 143.13
-impostoRam16 = 29.33
+ram16 = 155.42
+impostoRam16 = 31.22
 totalRam16 = round_up(ram16 + impostoRam16)
+ram32 = 249.60
+impostoRam32 = 51.14
+totalRam32 = round_up(ram32 + impostoRam32)
 ram = round_up(ram8 + impostoRam8 + ram16 + impostoRam16)
 gpu = 952.45
 
-componentes = [motherboard, gabinete, cpu, fonte650w, totalRam8, totalRam16, gpu]
-comprados = [gpu, cpu, gabinete]
+componentes = [motherboard, gabinete, cpu, fonte600w, gpu]
+comprados = [gpu, cpu, gabinete, motherboard]
 faltaComprar = [componente for componente in componentes if componente not in comprados]
 
 surplus = 0
 maxCartao = 1100
 livreCartao = 0
-faturaAtual = 458.90
-usoCheque = 177.58
-aParcela = 58.06
-aFaltam = 2
+faturaAtual = 536.54
+usoCheque = 63.61
+aParcela = 0
+aFaltam = 0
 bParcela = 191.90
-bFaltam = 3
+bFaltam = 2
 cParcela = 22.15
-cFaltam = 10
+cFaltam = 9
+dParcela = 16.54
+dFaltam = 4
+acadMes = 130
 
 duasContas = contaCondo + contaSky
 duasContasEx = f"({contaCondo} + {contaSky})"
-zeroContas = contaCondo2 + contaSky2
-zeroContasEx = f"{contaCondo2} + {contaSky2}"
 
 pcDict = {
-    "Placa mãe": motherboard, 
-    "fonte 650W": fonte650w,
-    "Pente de 16GB": totalRam16
+    "2X 8GB de RAM": totalRam16,
+    "fonte 600W": fonte600w
 }
 pcCartao = {
-    "Placa mãe": moboCartao, 
-    "fonte 650W": f650wCartao,
-    "Pente de 16GB": totalRam16
+    "2X 8GB de RAM": totalRam16,
+    "fonte 600W": fonte600w
 }
 
 
 avulso = 0
 entrada = 's'
 contLacos = 0
-cont = 1
+mesAtual = True
+cont = 2
+comprarParcela1 = []
+comprarParcela2 = []
+comprarParcela3 = []
+comprarParcela4 = []
 
 
 
@@ -118,10 +137,14 @@ while(entrada != 'n'):
     usoCheque = 0
     livreCheque = 0
     montante = salarioBruto + avulso
+    if mesAtual == True:
+        pagar = round_up(faturaAtual)
+        montante = 364.30 + avulso
+        mesAtual = False
     avulso = 0
     resto = round_up(montante - pagar)
     livreCartao += faturaAtual
-
+    
     if resto < 0 and resto >= -300:
         usoCheque = round_up(abs(resto))
         livreCheque = chequeEspecial - usoCheque
@@ -135,13 +158,10 @@ while(entrada != 'n'):
         avulso = resto
 
     for i in list(pcDict):
-        if contLacos > 0 and compravel:
+        if contLacos > 0 and contLacos < len(list(pcDict)) and compravel:
             virgula = ', '
-        else:
-            virgula = ''
-        if pcDict[i] <= avulso: # + livreCheque:
+        if pcDict[i] <= avulso + livreCheque:
             compravel.append(f'{virgula}{i} por {pcDict[i]}')
-            contLacos += 1
             avulso -= pcDict[i]
             if avulso < 0:
                 usoCheque = abs(avulso)
@@ -150,22 +170,29 @@ while(entrada != 'n'):
             cartaoOuPix.append('no pix')
             del pcDict[i]
             del pcCartao[i]
-        
+        contLacos += 1
 
     for i in list(pcCartao):
-        if contLacos > 0 and compravel:
+        if contLacos > 0 and contLacos < len(list(pcCartao)) and compravel:
             virgula = ', '
-        else:
-            virgula = ''
         if pcCartao[i] <= livreCartao:
             compravel.append(f'{virgula}{i} por {pcCartao[i]}')
-            contLacos += 1
             livreCartao -= pcCartao[i]
             comprar += pcCartao[i]
             cartaoOuPix.append('no cartão')
             del pcCartao[i]
-            del pcDict[i]
-        
+            del pcDict[i]    
+        contLacos += 1
+        '''for i in parcelaCompra(comprar, 5):
+            if not comprarParcela1:
+                comprarParcela1.append(i)
+            if not comprarParcela2:
+                comprarParcela2.append(i)
+            if not comprarParcela3:
+                comprarParcela3.append(i)
+            if not comprarParcela4:
+                comprarParcela4.append(i)'''
+
     
 
     if compravel and compravel != 'Tudo foi comprado':
@@ -173,8 +200,8 @@ while(entrada != 'n'):
             comprando += f'{compravel[i]} {cartaoOuPix[i]}'
 
     livreCartao = round_up(livreCartao)
-    printar = f'''Tanto que deve pagar: {pagar}
-Tanto que tem para pagar: {round_up(montante)}
+    printar = f'''Tanto que deve pagar: {pagar} ({faturaAtual} + Cheque Especial Usado + {duasContas})
+Tanto que tem para pagar: {montante}
 Tanto que sobrou: {resto}
 Tanto que usou do cheque especial: {usoCheque}
 Tanto que tem livre no cheque especial: {livreCheque}
@@ -184,13 +211,29 @@ Falta comprar: {list(pcDict)}
 
 '''
     print(printar)
-    aParcela = 58.06
     aFaltam -= 1
-    bParcela = 191.90
     bFaltam -= 1
-    cParcela = 22.15
     cFaltam -= 1
-    faturaAtual = round_up(proximaFatura(aParcela, aFaltam, bParcela, bFaltam, cParcela, cFaltam, comprar))
+
+    if not comprarParcela1:
+        comprarParcela1.append(0)
+    if not comprarParcela2:
+        comprarParcela2.append(0)
+    if not comprarParcela3:
+        comprarParcela3.append(0)
+    if not comprarParcela4:
+        comprarParcela4.append(0)
+
+    faturaAtual = round_up(proximaFatura(aParcela, aFaltam, bParcela, bFaltam, cParcela, cFaltam, comprarParcela1[0], comprarParcela2[0], comprarParcela3[0], comprarParcela4[0], acadMes))
+    if comprarParcela1:
+        comprarParcela1.pop(0)
+    if comprarParcela2:
+        comprarParcela2.pop(0)
+    if comprarParcela3:
+        comprarParcela3.pop(0)
+    if comprarParcela4:
+        comprarParcela4.pop(0)
+    
     cont += 1
     if cont == 12:
         cont = 0
